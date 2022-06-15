a42add32dac54ab650d2f8d3fc56e573e061c6a8
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-15 21:31:17 +0200

    Adding a class name.

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const faq = document.querySelector(".faq");
 const questions = document.querySelectorAll(".faq > li");
 
 // alert(faq.children.length);
-for (const question of faq.children) {
+for (const question of questions) {
+  //   alert(question.children[0].textContent);
   question.addEventListener("click", (e) => {
-    alert(e.target);
+    // alert(e.target);
+    // this.classlist.add("test");
+    // this.remove;
     // e.remove();
+    // alert(question);
+    question.classList.toggle("expanded");
   });
 }
+
+// alert("still");
