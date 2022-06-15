2149cd2684fd530f4c4a3c50239f342c0e457bb5
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-15 21:08:10 +0200

    Working on clicking.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("added");
+// alert("added");
+
+const faq = document.querySelector(".faq");
+const questions = document.querySelectorAll(".faq > li");
+
+// alert(faq.children.length);
+for (const question of faq.children) {
+  question.addEventListener("click", (e) => {
+    alert(e.target);
+    // e.remove();
+  });
+}
