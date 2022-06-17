4eb97b8786e33794c390789865cd7d1192f437d9
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-17 21:57:58 +0200

    Features.

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const questions = document.querySelectorAll(".faq > li");
 
 for (const question of questions) {
   question.addEventListener("click", (e) => {
-    question.classList.toggle("expanded");
+    if (question.classList.contains("expanded")) {
+      question.classList.remove("expanded");
+    } else {
+      for (const q of questions) {
+        q.classList.remove("expanded");
+      }
+      question.classList.add("expanded");
+    }
   });
 }
