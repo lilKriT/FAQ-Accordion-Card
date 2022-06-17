15209cedf4bce2d6f8a587ec0584bc92b70be266
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-17 21:22:47 +0200

    Cleaned some code.

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-// alert("added");
-
 const faq = document.querySelector(".faq");
 const questions = document.querySelectorAll(".faq > li");
 
-// alert(faq.children.length);
 for (const question of questions) {
-  //   alert(question.children[0].textContent);
   question.addEventListener("click", (e) => {
-    // alert(e.target);
-    // this.classlist.add("test");
-    // this.remove;
-    // e.remove();
-    // alert(question);
     question.classList.toggle("expanded");
   });
 }
-
-// alert("still");
